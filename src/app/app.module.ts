057f54b9a08138a9089aa4ea4dcae351a440f408
anadis764 <anadis764@gmail.com>
2021-10-28 23:14:42 +0530

    adding material module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
+import { MaterialModule } from './material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './user/pages/signup/signup.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
+   
+    
+   
   ],
   providers: [],
   bootstrap: [AppComponent]

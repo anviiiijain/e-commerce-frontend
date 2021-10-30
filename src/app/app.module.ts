c1d80fad2b518c44ad7f6ca9ec22e4f922df00d6
anadis764 <anadis764@gmail.com>
2021-10-30 10:43:32 +0530

    navbar-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
+import { NavbarComponent } from './user/navbar/navbar.component';
 import { MaterialModule } from './material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './user/pages/signup/signup.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
+    NavbarComponent
     
     
   ],
@@ -20,7 +21,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     SharedModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
+    
+
    
     
    

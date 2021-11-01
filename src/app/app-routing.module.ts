50b0bc17027e10bba0aa1d58787bedcf4b6cbb07
anviiiijain <anvijain250102@gmail.com>
2021-11-02 02:58:11 +0530

    [Story-102] - Login component and routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './user/pages/login/login.component';
+import { SignupComponent } from './user/pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'signup',
+    component:SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './user/pages/signup/signup.component';
+import { LoginComponent } from './user/pages/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    
+    LoginComponent,
     
   ],
   imports: [

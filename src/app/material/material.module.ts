b7d1e3ffac6b4f31da9fdd9a1a82ce631e3e8586
anviiiijain <anvijain250102@gmail.com>
2021-11-07 18:12:24 +0530

    [Story-105] - added review and Faq in product

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,7 +5,8 @@ import { MatListModule } from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatIconModule} from '@angular/material/icon'
+import {MatIconModule} from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 
 const MaterialComponents = [
@@ -16,6 +17,7 @@ const MaterialComponents = [
   MatButtonModule,
   MatCheckboxModule,
   MatIconModule,
+  MatExpansionModule,
 ];
 
 @NgModule({

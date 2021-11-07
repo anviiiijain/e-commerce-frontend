4c33d7aeaf02b85cb81c48b1a4379f3e414e1219
anviiiijain <anvijain250102@gmail.com>
2021-11-08 02:03:56 +0530

    AJ-made login and signup responsive

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { MaterialModule } from '../material/material.module';
-import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 
 @NgModule({
   declarations: [
-    ProductDetailsComponent
   ],
   imports: [
     CommonModule,

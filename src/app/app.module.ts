762150a8bc96e41197fc370543ba2074c3eac07d
AxithChoudhary <axithchoudhary18@gmail.com>
2021-11-09 19:04:20 +0530

    added homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import {HomepageComponent} from "./user/pages/homepage/homepage.component"
   declarations: [
     AppComponent,
     SignupComponent,
-    FooterComponent
+    FooterComponent,
     LoginComponent,
     ProductComponent,
     StarsComponent,

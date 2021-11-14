ac0556f72100863f54bc7f741e33cb94b3bbbaee
anviiiijain <anvijain250102@gmail.com>
2021-11-14 15:40:01 +0530

    integrated and added carousal dynamically

--- src/app/user/pages/homepage/homepage.component.ts
+++ src/app/user/pages/homepage/homepage.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/_services';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import flickity from "flickity";
 
 @Component({
   selector: 'app-homepage',
@@ -7,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
   stars=3.5;
-
-  constructor() { }
+  categories:any;
+  constructor(private _productService: ProductService,private snackbar:MatSnackBar,private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+    this._productService.getCategories().subscribe(data => {
+      this.categories= data.data;
+      this.cdr.detectChanges();
+      document.querySelectorAll(".catCarousel").forEach(card=>{
+        let flick=new flickity(card,{"draggable": false,"contain": true,"initialIndex": 1,"pageDots": false})
+      })
+      console.log("categories",this.categories)
+    }, error => {
+      console.log(error)
+    })
+  }
+
+  prepend(){
+    console.log("teest");
+    this.flick.prepend(`<div class="cat-card"></div>`);
   }
 
 }
